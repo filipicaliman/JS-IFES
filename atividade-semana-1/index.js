227c1144eb89de6aa3a739e839b86748c779bed2
Filipi Caliman <fgcaliman.dev@gmail.com>
2022-12-12 17:19:32 -0300

    js

---
+++ atividade-semana-1/index.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+
+// Leia os três números
+const n1 = prompt('Digite o número 1: ')
+const n2 = prompt('Digite o número 2: ')
+const n3 = prompt('Digite o número 3: ')
+// Calcule a soma e a média
+prompt = (`A soma é: $(n1+n2+n3)`)
+// Mostre na tela o valor da soma e média
